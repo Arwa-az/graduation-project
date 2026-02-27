715fcfd7c7f4be2752e76f22e32d305b282c266e
Pixel <pixel@webmail.com>
2026-02-27 05:14:09 +0300

    Add Favorite model with migration

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# جدول المعالم السياحية
 class Landmark(models.Model):  
     Destination = models.CharField(max_length=200)      # اسم المدينة
     Landmark_Name = models.CharField(max_length=200)      # اسم المعلم
@@ -8,3 +10,15 @@ class Landmark(models.Model):
 
 def __str__(self):
     return self.Landmark_Name
+
+# جدول المفضلة
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    landmark = models.ForeignKey("Landmark", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'landmark')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.landmark.Landmark_Name}"
