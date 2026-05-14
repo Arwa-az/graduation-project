21ca7ac8c766e6aa0c12a5cc5a5d01b3b52229dc
Arwa-az <arwa20044@hotmail.com>
2026-05-15 01:27:30 +0300

    add EMAIL_TIMEOUT

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ LOGOUT_REDIRECT_URL = '/'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'sauditales0@gmail.com'
-# EMAIL_HOST_PASSWORD = 'usyh cckg gsid ssgn'
+# EMAIL_HOST_PASSWORD = ''
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -52,6 +52,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
