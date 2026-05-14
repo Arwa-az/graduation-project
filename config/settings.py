3dc87e0a5d402fa5f7dd28f3b8e1ac6b092b1c9d
Arwa-az <arwa20044@hotmail.com>
2026-05-15 02:15:04 +0300

    update password reseting

--- config/settings.py
+++ config/settings.py
@@ -44,15 +44,14 @@ LOGOUT_REDIRECT_URL = '/'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'sauditales0@gmail.com'
-# EMAIL_HOST_PASSWORD = ''
+# EMAIL_HOST_PASSWORD = 'usyh cckg gsid ssgn'
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
