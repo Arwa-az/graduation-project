3060a719585a9cb5cf75b9f5c4d1e9593857bd63
Arwa-az <arwa20044@hotmail.com>
2026-05-15 23:15:35 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,8 @@ LOGOUT_REDIRECT_URL = '/'
 #for password reseting (using Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
